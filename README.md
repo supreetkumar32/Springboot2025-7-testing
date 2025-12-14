18b9398670bf53e0f51579386cb2bd57a7eb1000
Supreet <supreetkumar32@gmail.com>
2025-12-14 22:28:45 +0530

    assertj and assertions ,junit basics

--- README.md
+++ README.md
@@ -15,3 +15,33 @@ be used to reset each test case conditions.
 class. The method must be static. (Executed once)
 • @AfterAll: Indicates that the annotated method should be executed once after all test methods in the
 class. The method must be static. (Executed once)
+
+JUnit vs AssertJ
+JUnit is one of the most widely used testing frameworks in the Java ecosystem. JUnit
+provides a simple and standardized way to write test cases, execute them, and report
+the results.
+
+AssertJ, on the other hand, is not a testing framework but rather a library that
+complements testing frameworks like JUnit. It focuses on providing fluent and
+expressive assertions, enhancing the readability and maintainability of your test code.
+
+JUnit and AssertJ are both popular tools used in Java for testing, but they serve
+different purposes and have distinct features.
+
+Common AssertJ Methods
+1. Numbers:
+assertThat(5).isEqualTo(5).isNotEqualTo(10).isGreaterThan(4);
+2. String:
+assertThat("hello").startsWith("he").endsWith("lo").contains("ell");
+3. Boolean:
+assertThat(true).isTrue(); OR assertThat(false).isFalse();
+4. List/Array:
+assertThat(List.of("apple", "banana")).contains("apple")
+.doesNotContain("orange").hasSize(2);
+5. Exceptions:
+assertThatThrownBy(() -> {
+throw new IllegalArgumentException("Invalid argument");
+}).isInstanceOf(IllegalArgumentException.class)
+.hasMessage("Invalid argument")
+.hasStackTraceContaining("ExampleTest");
+More AssertJ Methods: https://assertj.github.io/doc/
--- src/test/java/com/weekseven/testing/demo/DemoApplicationTests.java
+++ src/test/java/com/weekseven/testing/demo/DemoApplicationTests.java
@@ -1,13 +1,75 @@
 package com.weekseven.testing.demo;
 
-import org.junit.jupiter.api.Test;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+//@SpringBootTest
+@Slf4j
 class DemoApplicationTests {
 
+	@BeforeEach
+	void setUp(){
+		log.info("It will run the method before each test");
+	}
+
+	@AfterEach
+	void tearDown(){
+		log.info("It will run the method after each test");
+	}
+
 	@Test
+	@Disabled
 	void contextLoads() {
+	}
+
+	@Test
+	@DisplayName("displayTestNameTwo")
+	void testNumbetOne() {
+		log.info("test one is run");
+	}
+
+	@BeforeAll
+	static void setUpOnce(){
+		log.info("it run at one time in the beginning");
+	}
+
+	@AfterAll
+	static void tearDownOnce(){
+		log.info("it run at one time in the last");
+	}
+
+	@Test
+	void testNumberTwo() {
+		log.info("test two is run");
+	}
+
+	@Test
+//	@DisplayName("displayTestNameTwo")
+	void testDivideTwoNumbers_whenDenominatorIsZero_ThenArithmeticException() {
+
+		int a = 5;
+		int b = 0;
+
+		assertThatThrownBy(() -> divideTwoNumbers(a, b))
+				.isInstanceOf(ArithmeticException.class)
+				.hasMessage("Tried to divide by zero");
+
+	}
+
+	int addTwoNumbers(int a, int b) {
+		return a+b;
+	}
+
+	double divideTwoNumbers(int a, int b) {
+		try {
+			return a/b;
+		} catch (ArithmeticException e) {
+			log.error("Arithmentic excepiton occured: "+e.getLocalizedMessage());
+			throw new ArithmeticException("Tried to divide by zero");
+		}
 	}
 
 }
