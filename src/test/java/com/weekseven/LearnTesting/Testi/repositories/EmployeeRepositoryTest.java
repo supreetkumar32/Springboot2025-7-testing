30b785a27a7d41dfed8873e6031e8ca8b2fa7c36
Supreet <supreetkumar32@gmail.com>
2025-12-18 10:48:07 +0530

    docker

--- src/test/java/com/weekseven/LearnTesting/Testi/repositories/EmployeeRepositoryTest.java
+++ src/test/java/com/weekseven/LearnTesting/Testi/repositories/EmployeeRepositoryTest.java
@@ -13,7 +13,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-//@Import(TestContainerConfiguration.class)
+@Import(TestContainerConfiguration.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class EmployeeRepositoryTest {
